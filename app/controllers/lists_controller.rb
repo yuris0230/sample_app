983b30876755198e1033e70ed70081c46cccc173
yuris0230 <178619823+yuris0230@users.noreply.github.com>
2025-09-12 21:16:23 +0000

    [Finish] 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,6 +12,22 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    # local var bcuz view dnt use it, update action then redirect
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+
   # 以下を追加
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
@@ -23,7 +39,7 @@ class ListsController < ApplicationController
   end
 
   private
-  # ストロングパラメータ
+  # ストロングパラメータ permit(:allow only these colum ONLY) 
   def list_params
     params.require(:list).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
 end
